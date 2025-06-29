4cf5c05b710552cc2744420b3205effd34251d4b
Adam-Yun <naelyunn@gmail.com>
2025-06-30 00:38:39 +1000

    Change file and folder structure

---
+++ app/src/app/components/introduction/introduction.tsx
@@ -0,0 +1,17 @@
+
+import "./introduction.css"
+
+
+const Introduction = () => {
+  return ( 
+    <div id="introduction-tag" className="introduction-container">
+        <div className="introduction-bg"></div>
+        <div className="introduction-overlay"></div>
+        <div className="introduction-content">
+          <h2 className="introduction-title">ENCORE STUDIO</h2>
+        </div>
+    </div>
+  );
+};
+
+export default Introduction;
---
+++ app/src/app/components/showcase/showcase.tsx
@@ -0,0 +1,177 @@
+
+import "./showcase.css"
+import Image from 'next/image';
+
+
+const Showcase = () => {
+
+  // Array images that contains the path to the images
+  const images = [
+    "/Images/sample_image_1.jpg",
+    "/Images/sample_image_2.jpg",
+    "/Images/sample_image_3.jpg",
+    "/Images/sample_image_4.jpg",
+    "/Images/sample_image_5.jpg",
+    "/Images/sample_image_6.jpg",
+    "/Images/sample_image_7.jpg",
+    "/Images/sample_image_8.jpg",
+    "/Images/sample_image_9.jpg",
+    "/Images/sample_image_10.jpg"
+  ]
+
+  
+  return ( 
+    <div id="portfolio-tag" className="portfolio-container">
+        <ol className="cards-track">
+
+          <li className="card">
+            <Image className="card-image" src={images[0]} width={800} height={600} alt="photoshoots" priority/>
+          </li>
+
+          <li className="card">
+            <Image className="card-image" src={images[1]} width={800} height={600} alt="photoshoots" priority/>
+          </li>
+
+          <li className="card">
+            <Image className="card-image" src={images[2]} width={800} height={600} alt="photoshoots" priority/>
+          </li>
+
+          <li className="card">
+            <Image className="card-image" src={images[3]} width={800} height={600} alt="photoshoots" priority/>
+          </li>
+
+          <li className="card">
+            <Image className="card-image" src={images[4]} width={800} height={600} alt="photoshoots" priority/>
+          </li>
+
+          <li className="card">
+            <Image className="card-image" src={images[5]} width={800} height={600} alt="photoshoots" priority/>
+          </li>
+          
+          <li className="card">
+            <Image className="card-image" src={images[6]} width={800} height={600} alt="photoshoots" priority/>
+          </li>
+
+          <li className="card">
+            <Image className="card-image" src={images[7]} width={800} height={600} alt="photoshoots" priority/>
+          </li>
+
+          <li className="card">
+            <Image className="card-image" src={images[8]} width={800} height={600} alt="photoshoots" priority/>
+          </li>
+
+          <li className="card">
+            <Image className="card-image" src={images[9]} width={800} height={600} alt="photoshoots" priority/>
+          </li>
+
+        </ol>
+    </div>
+  );
+};
+
+export default Showcase;
+
+
+
+// import "./portfolio.css"
+// import Carousel from './carousel';
+// import { useState, useEffect } from "react";
+
+
+// const Portfolio = () => {
+
+//   // Array images that contains the path to the images
+//   const images = [
+//     "/Images/sample_image_1.jpg",
+//     "/Images/sample_image_2.jpg",
+//     "/Images/sample_image_3.jpg",
+//     "/Images/sample_image_4.jpg",
+//     "/Images/sample_image_5.jpg",
+//     "/Images/sample_image_6.jpg",
+//     "/Images/sample_image_7.jpg",
+//     "/Images/sample_image_8.jpg",
+//     "/Images/sample_image_9.jpg",
+//     "/Images/sample_image_10.jpg"
+//   ]
+
+//   const alphabets = [
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//     "G",
+//     "H"
+//   ]
+
+//   const [indexX, setIndexX] = useState(0);
+//   const [indexY, setIndexY] = useState(1);
+//   const [indexZ, setIndexZ] = useState(2);
+//   const [indexW, setIndexW] = useState(3);
+
+//   useEffect(() => {
+//     let x = 0;
+//     let y = 1;
+//     let z = 2;
+//     let w = 3;
+//     const interval = setInterval(() => {
+//       setIndexX(x)
+//       setIndexY(y)
+//       setIndexZ(z)
+//       setIndexW(w)
+//       x += 1
+//       y += 1
+//       z += 1
+//       w += 1
+//       x = checkSeven(x)
+//       y = checkSeven(y)
+//       z = checkSeven(z)
+//       w = checkSeven(w)
+//     }, 2000);
+
+//     // Clean up interval on component unmount
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   // Checks if the number is 7 set to 0 if true, dont bother otherwise
+//   const checkSeven = (x: number) => {
+//     if(x > 6){
+//       x = 0
+//     }
+//     return x
+//   }
+  
+//   return ( 
+//     <div id="portfolio-tag" className="portfolio-container">
+//         <ol className="cards-container">
+//           <li className="card-container">
+//             <Carousel images={images} index={indexX}/>
+//             {/* <div className="card">
+//               <Carousel images={alphabets} index={indexX}/>
+//             </div> */}
+//           </li>
+//           <li className="card-container">
+//             <Carousel images={images} index={indexY}/>
+//             {/* <div className="card">
+//               <Carousel images={alphabets} index={indexY}/>
+//             </div> */}
+//           </li>
+//           <li className="card-container">
+//             <Carousel images={images} index={indexZ}/>
+//             {/* <div className="card">
+//               <Carousel images={alphabets} index={indexZ}/>
+//             </div> */}
+//           </li>
+//           <li className="card-container">
+//             <Carousel images={images} index={indexW}/>
+//             {/* <div className="card">
+//               <Carousel images={alphabets} index={indexZ}/>
+//             </div> */}
+//           </li>
+//         </ol>
+//     </div>
+//   );
+// };
+
+// export default Portfolio;
